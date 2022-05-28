04c094e52fd63f16eb7ce317cd99d23a57babbec
LizzySawyer <105354784+LizzySawyer@users.noreply.github.com>
2022-05-28 21:43:07 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+
+
+
+class Student:
+    # [assignment] Skeleton class. Add your code here
+    def __init__(self, name, age, tracks, score):
+        self.name = str(name)
+        self.age = int(age)
+        self.tracks = list(tracks)
+        self.score = float(score)
+
+
+        def change_name(self, name):
+             self.new_name = name
+             
+        def change_age(Bob, change_age):
+            self.age = new_age
+            
+            
+        def add_track(self, tracks):
+            self.tracks.append()
+    
+        def get_score(self):
+            return ()
+
+
+
+Bob = Student('Robert', 25 , ["FDF","BIG"] , 20.90)       
+
+# Expected methods
+Bob.change_name="Peter"
+Bob.change_age=34
+Bob.add_track=("UI/UX")
+Bob.get_score= Bob.score
+
+print (Bob.name)
+print (Bob.age)
+print (Bob.tracks)
+print (Bob.score)
+print ("The student new name is", Bob.change_name)
+print ("The student new age is", Bob.change_age)
+print ("The student new track is", Bob.add_track)
+print ("The student score is", Bob.get_score)
+
